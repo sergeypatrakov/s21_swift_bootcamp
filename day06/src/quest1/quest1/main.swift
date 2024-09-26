2bfca4f26b11dab3f069c884bbf7c5be006bbe76
Sergey <patrakovsergo@gmail.com>
2024-09-26 23:19:49 +0400

    start woking on day06

---
+++ day06/src/quest1/quest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  quest1
+//
+//  Created by Sergey Patrakov on 26.09.2024.
+//
+
+import Foundation
+import RealmSwift
+
+print("Hello, World!")
+print("THANK YOU \u{2764} ANTON!")
