f76b66282677cd956385791eaac96c3689e45be3
Sergey <patrakovsergo@gmail.com>
2024-10-01 11:44:45 +0300

    day07 start

---
+++ day07/src/quest0/quest0/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  quest0
+//
+//  Created by Sergey Patrakov on 01.10.2024.
+//
+
+import Foundation
+
+// Создать новый проект в XCode
+
+print("Very hard task...")
+
---
+++ day07/src/quest1/quest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  quest1
+//
+//  Created by Sergey Patrakov on 01.10.2024.
+//
+
+import Foundation
+import URLSession
+
+print("Hello, World!")
+
